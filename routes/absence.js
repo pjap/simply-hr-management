e6af8921191439f45e624a6002d768324c02ea9c
pjap <pranajaya.pratama@gmail.com>
2017-10-11 20:12:29 +0700

    ADD ABSENCES AND FIX STYLING

--- routes/absence.js
+++ routes/absence.js
@@ -5,31 +5,126 @@ const Model = require('../models')
 
 router.get('/', function(req,res) {
   Model.Absence.findAll({
+    attributes: [['id','id'],['date','date'],['checkin','checkin'],['checkout','checkout'],['EmployeeId','EmployeeId'],
+                  ['RuleId','RuleId'],['JobPositionId','JobPositionId']],
+    include: [Model.Employee, Model.Rule, Model.JobPosition]
   })
   .then(dataAbsence => {
-    console.log(dataAbsence);
-    // res.render('absence/absence', {dataAbsence:dataAbsence})
-    res.send(dataAbsence)
+    Model.Employee.findAll()
+    .then(dataEmployee => {
+      Model.Rule.findAll()
+      .then(dataRule => {
+        Model.JobPosition.findAll()
+        .then(dataJobPosition => {
+          // console.log(dataAbsence);
+          // res.send(dataAbsence)
+          res.render('absence/absence', {dataEmployee: dataEmployee, dataAbsence:dataAbsence,dataRule: dataRule,dataJobPosition:dataJobPosition, dataError: null})
+        })
+      })
+    })
   })
 })
 
 router.post('/add',(req,res)=>{
-  Model.Absence.create({
-    date: req.body.date,
-    checkin: req.body.checkin,
-    checkout: req.body.checkout,
-    EmployeeId: req.body.EmployeeId,
-    RuleId: req.body.RuleId,
-    JobPositionId: req.body.JobPositionId
+  Model.Absence.findOne({
+    where : {
+      date: req.body.date,
+      EmployeeId: req.body.EmployeeId
+    }
   })
-  .then(dataAbsence=>{
+  .then(data => {
+    if(data) {
+      Model.Absence.findAll({
+        attributes: [['id','id'],['date','date'],['checkin','checkin'],['checkout','checkout'],['EmployeeId','EmployeeId'],
+                      ['RuleId','RuleId'],['JobPositionId','JobPositionId']],
+        include: [Model.Employee, Model.Rule, Model.JobPosition]
+      })
+      .then(dataAbsence => {
+        Model.Employee.findAll()
+        .then(dataEmployee => {
+          Model.Rule.findAll()
+          .then(dataRule => {
+            Model.JobPosition.findAll()
+            .then(dataJobPosition => {
+              // console.log(dataAbsence);
+              // res.send(dataAbsence)
+              res.redirect('/absence')
+            })
+          })
+        })
+      })
+    } else {
+      Model.Absence.create({
+        date: req.body.date,
+        checkin: req.body.checkin,
+        checkout: req.body.checkout,
+        EmployeeId: req.body.EmployeeId,
+        RuleId: req.body.RuleId,
+        JobPositionId: req.body.JobPositionId
+      })
+      .then(dataAbsence=>{
+        res.redirect('/absence')
+        // res.send(dataAbsence)
+      })
+      .catch(err=>{
+        res.send(err)
+      })
+    }
+  })
+})
+
+router.get('/delete/:id', function(req,res) {
+  Model.Absence.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(() => {
     res.redirect('/absence')
-    // res.send(dataAbsence)
   })
-  .catch(err=>{
+  .catch(err => {
     res.send(err)
   })
 })
+
+
+router.get('/edit/:id', function(req,res) {
+  Model.Absence.findAll({
+    attributes: [['id','id'],['date','date'],['checkin','checkin'],['checkout','checkout'],['EmployeeId','EmployeeId'],
+                  ['RuleId','RuleId'],['JobPositionId','JobPositionId']],
+    include: [Model.Employee, Model.Rule, Model.JobPosition]
+  })
+  .then(dataAbsence => {
+    Model.Employee.findAll()
+    .then(dataEmployee => {
+      Model.Rule.findAll()
+      .then(dataRule => {
+        Model.JobPosition.findAll()
+        .then(dataJobPosition => {
+          // console.log(dataAbsence);
+          // res.send(dataAbsence)
+          res.render('absence/edit', {dataEmployee: dataEmployee, dataAbsence:dataAbsence,dataRule: dataRule,dataJobPosition:dataJobPosition, dataError: null})
+        })
+      })
+    })
+  })
+})
+
+router.get('/report', function(req,res) {
+  Model.Absence.findAll({
+    attributes: [['id','id'],['date','date'],['checkin','checkin'],['checkout','checkout'],['EmployeeId','EmployeeId'],
+                  ['RuleId','RuleId'],['JobPositionId','JobPositionId']],
+    include: [Model.Employee, Model.Rule, Model.JobPosition]
+  })
+  .then(dataAbsence => {
+    res.send(dataAbsence)
+  })
+  .catch(err => {
+    res.send(err)
+  })
+})
+
+
 
 
 
